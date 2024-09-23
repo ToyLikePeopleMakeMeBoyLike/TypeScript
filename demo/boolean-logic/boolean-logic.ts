643a9a45dec6c91505011ceecd94b418d116b385
luislous123 <33298190+luislous123@users.noreply.github.com>
2024-09-23 20:50:59 +0000

    modified:   demo/boolean-logic/boolean-logic.ts 	modified:   demo/compound-bool/compound-bool.ts 	modified:   pnpm-lock.yaml 	new file:   ztm-typescript

--- demo/boolean-logic/boolean-logic.ts
+++ demo/boolean-logic/boolean-logic.ts
@@ -1,8 +1,29 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+import { isGeneratorObject } from "util/types";
 
 // Boolean logic is a fundamental concept that involves evaluating true/false
 // statements. It is a system of logical thought that uses true and false
 // values to make deductions or draw conclusions. Boolean logic is used to
 // express conditions and make decisions based on the truth or falsehood of
 // these conditions.
+
+
+
+
+
+//const yes =true;
+//const no = false;
+
+//const age = 18;
+//const canPurchase = age >= 18;
+
+//assert.equal(canPurchase, false);
+
+
+const two =2;
+const isThree = (1+ two) ===3;  //Super equal?
+assert.equal(isThree, true);
+
+const isNotThree =(1+ two) !== 10;
+assert.equal(isNotThree, true);
--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,3 +1,15 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+
+//NOT
+const writing =true;
+const reading =!writing;
+
+
+// OR
+
+const rating =9;
+const favoriteMovie = false;
+
+const suggestMovie = rating > 8 || favoriteMovie;
