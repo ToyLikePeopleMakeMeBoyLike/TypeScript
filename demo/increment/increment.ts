4a832ffbfb60325a224a6f6c202573f82d552f6b
luislous123 <33298190+luislous123@users.noreply.github.com>
2024-10-15 23:25:29 +0000

    modified:   demo/increment/increment.ts

--- demo/increment/increment.ts
+++ demo/increment/increment.ts
@@ -7,3 +7,21 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+let n =1 ;
+n++;
+assert.equal(n, 2);
+//prefixx increment
+++n;
+assert.equal(n, 3);
+
+n =5;
+const k = n++;
+assert.equal(k, 6);
+
+
+n =5 ;
+const t = --n;
+assert.equal(t, 4);
